40174b8fb331a6be9d3d8c75aaad065f785ca0b7
Nicholas Zafiriou <nicholaszafiriou@gmail.com>
2020-02-20 12:11:12 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,8 +2,16 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+  @@uniq_b = []
+
   def initialize(brand)
     @brand = brand
+    @@uniq_b << brand
+    @@uniq_b = @@uniq_b.uniq
+    if @@uniq_b[-1] == brand
+      BRANDS << brand
+    end
   end
 
   def cobble
